9d08f7bbc11610fc920df7755b9c3c175d8463b6
Humberto Marino Oliveira <humberto@deft-one.com>
2021-07-25 03:17:10 -0300

    Ajustes build.sh

--- build.sh
+++ build.sh
@@ -12,13 +12,18 @@ multipass launch -n $VM_NAME -c2 -m2g -d5g --cloud-init ./cloud-config.yaml
 #multipass shell $VM_NAME
 VM_ADDRESS=$(multipass list |awk '{ print $3 }'| awk 'NR!=1 {print}')
 
-if [ ! -f $ANSIBLE_MULTIPASS_INVENTORY ]
+if [ ! -f ${ANSIBLE_MULTIPASS_INVENTORY} ]
 then
-    touch $ANSIBLE_MULTIPASS_INVENTORY
-    echo "[multipass]" > $ANSIBLE_MULTIPASS_INVENTORY    
+    touch ${ANSIBLE_MULTIPASS_INVENTORY}
+    echo "[multipass]" > ${ANSIBLE_MULTIPASS_INVENTORY}  
 fi
 
 NEW_LINE="$VM_NAME ansible_host=$VM_ADDRESS ansible_ssh_user=ubuntu"
-sed -i 'Ns/.*$VM_NAME.*/$NEW_LINE/' $ANSIBLE_MULTIPASS_INVENTORY
+
+if [ 'grep $VM_NAME ${ANSIBLE_MULTIPASS_INVENTORY}' ] ; then 
+  sed -i 'Ns/.*$VM_NAME.*/$NEW_LINE/' ${ANSIBLE_MULTIPASS_INVENTORY}
+else 
+  echo $NEW_LINE >> ${ANSIBLE_MULTIPASS_INVENTORY}
+fi
 
 ansible-playbook $ANSIBLE_PLAYBOOKS/setup-multipass.yml --limit $VM_ADDRESS
