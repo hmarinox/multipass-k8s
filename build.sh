efa5c76df724e3e07c44e8b3b3673614db8b7e3e
Humberto Marino Oliveira <humberto@deft-one.com>
2021-07-25 02:42:18 -0300

    Ajustes build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,5 @@ ANSIBLE_PLAYBOOKS=$ANSIBLE_WORKDIR/playbooks
 multipass delete $VM_NAME --purge
 multipass launch -n $VM_NAME -c2 -m2g -d5g --cloud-init ./cloud-config.yaml
 #multipass shell $VM_NAME
-ansible-playbook $ANSIBLE_PLAYBOOKS/setup-multipass.yml 
+VM_ADDRESS=$(multipass list |awk '{ print $3 }'| awk 'NR!=1 {print}')
+ansible-playbook $ANSIBLE_PLAYBOOKS/setup-multipass.yml --limit $VM_ADDRESS
