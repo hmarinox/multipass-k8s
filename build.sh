6476d21669d53d601a61c86391daaa6cd07863c3
Humberto Marino Oliveira <humberto@deft-one.com>
2021-07-25 02:24:38 -0300

    Ajustes build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 VM_NAME=$1
 
 multipass delete $VM_NAME --purge
-multipass launch -n $VM_NAME -c2 -m2g -d5g
+multipass launch -n $VM_NAME -c2 -m2g -d5g --cloud-init ./cloud-config.yaml
 multipass shell $VM_NAME
