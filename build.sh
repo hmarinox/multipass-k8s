3d7d4bf4be6ba9969a8ff154a51afb03064456e3
Humberto Marino Oliveira <humberto@deft-one.com>
2021-07-25 02:32:40 -0300

    Ajustes build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 VM_NAME=$1
 
+MULTIPASS_WORKDIR=/Users/hmarino/WORKSPACES/LINUXTIPS/descomplicando-kubernetes/k8s
+ANSIBLE_WORKDIR=/Users/hmarino/WORKSPACES/ansible
+ANSIBLE_PLAYBOOKS=$ANSIBLE_WORKDIR/playbooks
+
+
 multipass delete $VM_NAME --purge
 multipass launch -n $VM_NAME -c2 -m2g -d5g --cloud-init ./cloud-config.yaml
-multipass shell $VM_NAME
+#multipass shell $VM_NAME
+ansible-playbook $ANSIBLE_PLAYBOOKS/setup-multipass.yml 
