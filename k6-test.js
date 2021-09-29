2002c0e15f0a9bacd60e952bbe15d8ee3d257907
Humberto Marino Oliveira <humberto@deft-one.com>
2021-09-29 00:22:08 -0300

    stress with k6

---
+++ k6-test.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '1m30s', target: 100 },
+    { duration: '5m30s', target: 256 },
+    { duration: '20s', target: 0 },
+  ],
+};
+
+export default function () {
+  let res = http.get('http://172.16.29.52:32333/');
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  //sleep(1);
+}
